f33d8ed69cc822f37738fc421742bac807f1350b
GCano75 <51148227+GCano75@users.noreply.github.com>
2019-05-30 11:39:46 +0200

    Delete 31.16 Extended Euclid.py

--- 31.16 Extended Euclid.py
+++
@@ -1,8 +0,0 @@
-def Ext(a,b):
-    """ algorithme d'Euclide étendu. Les nombres a at b doivent être des entiers naturels. L'algorithme renvoie d=pgcd(a,b) et des entiers x et y tels que d=xa+yb."""
-    if b==0:
-        return(a,1,0)
-    else:
-        (D,X,Y)=Ext(b,a%b)
-        Q=a//b
-        return((D,Y,X-Y*Q))
